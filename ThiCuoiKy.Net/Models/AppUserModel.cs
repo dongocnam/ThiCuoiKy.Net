c291dad14f696386386e3974e7c04502c08d8ea3
dongocnam <dongocnam0000@gmail.com>
2024-06-15 19:25:21 +0700

    update logic

---
+++ ThiCuoiKy.Net/Models/AppUserModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ThiCuoiKy.Net.Models
+{
+    public class AppUserModel : IdentityUser
+    {
+        public string Occupation { get; set; }
+    }
+}
